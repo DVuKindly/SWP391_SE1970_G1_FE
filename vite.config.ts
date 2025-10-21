514e1c7371783e4acb982953828bdb1459848f97
Nguyen Thanh Phuc <phucnthe170081@gmail.com>
2025-10-21 10:59:40 +0700

    update gui thong tin thanh toan

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://localhost:7281',
+        target: 'http://localhost:7281',
         changeOrigin: true,
         secure: false,
       },
